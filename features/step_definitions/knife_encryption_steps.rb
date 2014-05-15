0a418c656abd0d279927c49656fbbcfeb4adedef
Chris Griego <cgriego@gmail.com>
2014-05-15 12:19:28 -0500

    Fix warnings from redefined constant in tests

--- features/step_definitions/knife_encryption_steps.rb
+++ features/step_definitions/knife_encryption_steps.rb
@@ -1,10 +1,10 @@
 Then(/^the stdout should be one of:$/) do |table|
-  LATEST_CHEF = "11.12.4"
+  latest_chef = "11.12.4"
   # table is a Cucumber::Ast::Table
   data = table.rows_hash
   begin
-    puts "unknown Chef version, attempting #{LATEST_CHEF} encrypted string"
-    data[Chef::VERSION] = data[LATEST_CHEF]
+    puts "unknown Chef version, attempting #{latest_chef} encrypted string"
+    data[Chef::VERSION] = data[latest_chef]
   end unless data[Chef::VERSION]
 
   step("the stdout should contain exactly:", data[Chef::VERSION] ? data[Chef::VERSION] : data["11.12.4"])
